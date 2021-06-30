02d978bd39dbe1f1fefb9b3ca8d531688b42b35c
Michael Gellis <mgellis@cpucodeschool.com>
2021-06-30 21:36:31 +0000

    session work on the worksheets

--- Sessions/01/1 worksheet.sql
+++ Sessions/01/1 worksheet.sql
@@ -2,23 +2,28 @@ use RecordKeeperDB
 go
 --worldrecords
 --1 show all 
-
+select * from WorldRecord w
 --2 show all but only columns WorldRecordId, RecordName, and FullName
-
+select w.WorldRecordId, w.RecordName, w.FullName from WorldRecord w
 --3 show same as above but columns in this order WorldRecordId, FullName, RecordName
-
+select w.WorldRecordId, w.FullName, w.RecordName from WorldRecord w
 --4 show all for the Year Achieved 2017
-
+select * from WorldRecord w where w.YearAchieved = 2017
 --5 show all that are Year Achieved before 2000
-
+select * from WorldRecord w where w.YearAchieved < 2000
 --6 show all that are Year Achieved after 2000
-
+select * from WorldRecord w where w.YearAchieved > 2000
 --7 show all that are not for the Year Achieved 2000
-
+select * from WorldRecord w where w.YearAchieved <> 2000
 --8 show all that were achieved in the years 2000, 2016, 2018
-
+select * from WorldRecord w where w.YearAchieved in (2000,2016,2018)
 --9 show all that are between the years 2000 and 2012
-
+select * from WorldRecord w where w.YearAchieved between 2000 and 2012
 --10 show all sorted by Year Achieved to earliest to latest
-
---11 show all with an Amount from 1 to 10 sorted by the Amount (high to low) and then by the Year Achieved (low to high)
+select * from WorldRecord w order by w.YearAchieved
+--11 show all with an Amount from 1 to 10 sorted by the Amount (high to low) 
+--and then by the Year Achieved (low to high)
+select * 
+from WorldRecord w 
+where w.Amount between 1 and 10
+order by w.Amount desc, w.YearAchieved 
--- Sessions/01/2 worksheet.sql
+++ Sessions/01/2 worksheet.sql
@@ -1,20 +1,29 @@
 use RecordKeeperDB
 go
---Invention
+--worldrecords
 --1 show all 
-
---2 show only columns InventionId, YearInvented, and InventionName sorted by "YearInvented"
-
---3 show all for Year Invented in the 1800s sorted by Year Invented from most recent to oldest
-
---4 show only the columns InventionId, InventionName, InventorLastName sorted by Country and for the Year Invented 1440, 1605,1733, 1804, and 1901
-
---5 show all where inventor was born in the 1800s
-
---6 show all where inentor died before 2000
-
---7 show all inventions after 1789 sorted by Last Name from Z to A
-
---8 show all where the inventor was less than 30 years old at time of invention
-
-
+select * from WorldRecord w
+--2 show all but only columns WorldRecordId, RecordName, and FullName
+select w.WorldRecordId, w.RecordName, w.FullName from WorldRecord w
+--3 show same as above but columns in this order WorldRecordId, FullName, RecordName
+select w.WorldRecordId, w.FullName, w.RecordName from WorldRecord w
+--4 show all for the Year Achieved 2017
+select * from WorldRecord w where w.YearAchieved = 2017
+--5 show all that are Year Achieved before 2000
+select * from WorldRecord w where w.YearAchieved < 2000
+--6 show all that are Year Achieved after 2000
+select * from WorldRecord w where w.YearAchieved > 2000
+--7 show all that are not for the Year Achieved 2000
+select * from WorldRecord w where w.YearAchieved <> 2000
+--8 show all that were achieved in the years 2000, 2016, 2018
+select * from WorldRecord w where w.YearAchieved in (2000,2016,2018)
+--9 show all that are between the years 2000 and 2012
+select * from WorldRecord w where w.YearAchieved between 2000 and 2012
+--10 show all sorted by Year Achieved to earliest to latest
+select * from WorldRecord w order by w.YearAchieved
+--11 show all with an Amount from 1 to 10 sorted by the Amount (high to low) 
+--and then by the Year Achieved (low to high)
+select * 
+from WorldRecord w 
+where w.Amount between 1 and 10
+order by w.Amount desc, w.YearAchieved 
