2a6c1d196e793e75f42b97fb3586f0206c960889
Michael Gellis <mgellis@cpucodeschool.com>
2021-06-30 21:24:11 +0000

    prep

--- Sessions/01/HW.sql
+++ Sessions/01/HW.sql
@@ -5,8 +5,19 @@ go
 
 --2 show all but only columns Olympic Year, Country and Medal
 
---3 show all medalists that won their medal in the 1800s
+--3 show all that won their medal in the 1800s
 
---4 show all medalists that were over 50 years old when they won their medal sorted by Year Born
+--4 show all that were over 50 years old when they won their medal sorted by Year Born (high to low)
 
---5 show all medalists for the Olymmpic Years 1896, 1998, 2008 sorted by Olympic Year (high to low), Last Name (low to high)
+--5 show all for the Olymmpic Years 1896, 1998, 2008 sorted by Olympic Year (high to low), Last Name (low to high)
+
+--6 show all sorted (all low to high) by Country, Olympic Year, and then by Year Born, that were born in the 1800s. In the resultset only include the Last Name and the columns that are in the sort.
+
+--7 show all that were in their 30s when they won their medal sorted by Year Born from highest to lowest
+
+--8 show all that won their medals when they were either 20,30,40, or 50 years old
+
+--invention
+--9 show all inventions that as of the current year are over 300 years old
+
+--10 show all inventions that the inventor died before they reached 50 years old
