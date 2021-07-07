3d0dd175d2820afcbd165e65d749bedca99598a5
Michael Gellis <mzgellis@hotmail.com>
2021-07-07 16:39:41 -0400

    session prep

---
+++ Sessions/02/HW.sql
@@ -0,0 +1,25 @@
+--1 show me all medals won in the summer season in Greece or Spain sorted by sport and then year
+
+/*2
+The Swiss government has requested a list of medalists from their country since 1928 who were over the age of 24 when they won their medal.
+Include only First name, last name, age when they won the medal sorted by sport and sport category.
+*/
+
+/*3
+For all medalists that won a gold medal for a some sort of skating show their first name, last name, olympic location, sport and year.
+Sort by year and sport.
+*/
+
+/*4
+There is some suspicion on certain medalists. 
+A clue of foul play was found for silver and gold medalists under 30 years old with the letters "st" in their last name, followed by an "e", there may be other letters between "st" and "e".  
+Provide the a list of medalists that match that clue, exclude the MedalistId, Code, and Year Born, include the age
+Sort by age high to low, medal, last name
+*/
+
+/*5
+We need to produce a list of inventions that were either created for war or were invented because of a war. 
+Can you produce a list of inventions that mention war in their description. 
+Be careful not to include descriptions that have words like hardware or forward, but do include words that begin with war like warhead. 
+We may get some that are not related, like if has "wart", that is fine, we will remove those manually (no pun intended)
+*/
