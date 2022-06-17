0cf1fb477c8015515f022d64910c64c55d4f9b5b
Michael Gellis <mgellis@cpucodeschool.com>
2022-06-17 19:31:20 +0000

    Maui

---
+++ Software/HelloWorld/HelloWorldMAUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World my name is Michael Gellis " + DateTime.Now.TimeOfDay.ToString();
+        }
+    }
+}
