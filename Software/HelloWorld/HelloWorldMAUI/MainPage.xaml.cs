137819294c806d8b31e731f7749ca246ea3e7657
Michael Gellis <mgellis@cpucodeschool.com>
2022-06-19 21:22:01 +0000

    session work

---
+++ Software/HelloWorld/HelloWorldMAUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           label1.Text = "MG Hello World! " + DateTime.Now.TimeOfDay.ToString();
+        }
+    }
+}
