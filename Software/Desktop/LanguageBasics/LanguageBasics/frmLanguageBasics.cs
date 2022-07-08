c21a3269db6bc2bb89be02430b4cd79d8ae909df
Michael Gellis <mgellis@cpucodeschool.com>
2022-07-08 00:36:15 +0000

    session complete

--- Software/Desktop/LanguageBasics/LanguageBasics/frmLanguageBasics.cs
+++ Software/Desktop/LanguageBasics/LanguageBasics/frmLanguageBasics.cs
@@ -17,6 +17,27 @@ namespace LanguageBasics
         {
             InitializeComponent();
             //event subscription goes here
+            btnEventHandler1.Click += BtnEventHandler1_Click;
+            btnEventHandler2.MouseMove += BtnEventHandler2_MouseMove;
+            btnEventHandler2.MouseLeave += BtnEventHandler2_MouseLeave;
+        }
+
+        private void BtnEventHandler2_MouseLeave(object? sender, EventArgs e)
+        {
+            btnEventHandler2.BackColor = SystemColors.Control;
+            btnEventHandler2.ForeColor = SystemColors.ControlText;
+        }
+
+        private void BtnEventHandler2_MouseMove(object? sender, MouseEventArgs e)
+        {
+            txtOutput.Text = txtOutput.Text + "Mouse moved over BtnEventHandler2 " + DateTime.Now.TimeOfDay.ToString() + Environment.NewLine;
+            btnEventHandler2.BackColor = Color.DodgerBlue;
+            btnEventHandler2.ForeColor = Color.OrangeRed;
+        }
+
+        private void BtnEventHandler1_Click(object? sender, EventArgs e)
+        {
+            txtOutput.Text = txtOutput.Text + "BtnEventHandler1 was clicked " + DateTime.Now.TimeOfDay.ToString() + Environment.NewLine;
         }
 
         //code goes here
