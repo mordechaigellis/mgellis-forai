d6bb7e7f2560682c353c4ea07f069133e5349f71
mordechaigellis <mgellis@cpucodeschool.com>
2023-01-19 22:24:20 +0000

    2-16-6 finished recording

--- RecordKeeper/RecordKeeperTest/PresidentTest.cs
+++ RecordKeeper/RecordKeeperTest/PresidentTest.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Data.SqlClient;
 
 namespace RecordKeeperTest
 {
@@ -9,6 +10,54 @@ namespace RecordKeeperTest
         {
             DBManager.SetConnectionString("Server=.\\SQLExpress;Database=RecordKeeperDB;Trusted_Connection=true");
             
+        }
+
+
+        [Test]
+        [TestCase("Sam", "Test1", "2035-1-1", 2075)]
+        [TestCase("Sam", "Test2", "1800-1-1", 1836)]
+        public void InsertNewPresident(string firstname, string lastname, DateTime dateborn, int termstart) {
+            DataTable dt = SQLUtility.GetDataTable("select * from president where presidentid = 0");
+            DataRow r = dt.Rows.Add();
+            Assume.That(dt.Rows.Count == 1);
+            int partyid = SQLUtility.GetFirstColumnFirstRowValue("select top 1 partyid from party");
+            Assume.That(partyid > 0, "Can't run test no parties in the db");
+            int maxnum = SQLUtility.GetFirstColumnFirstRowValue("select max(num) from president");
+            
+            maxnum = maxnum + 1;
+
+            TestContext.WriteLine("insert president with num = " + maxnum);
+
+            r["partyid"] = partyid;
+            r["Num"] = maxnum;
+            r["FirstName"] = firstname;
+            r["LastName"] = lastname;
+            r["DateBorn"] = dateborn;
+            r["TermStart"] = termstart;
+            President.Save(dt);
+
+            int newid = SQLUtility.GetFirstColumnFirstRowValue("select * from president where num = " + maxnum);
+
+            Assert.IsTrue(newid > 0, "president with num = " + maxnum + " is not found in db");
+            TestContext.WriteLine("President with " + maxnum + " is found in db with pk value = " + newid);
+        }
+
+        [Test]
+        public void ChangeExistingPresidentTermStart() {
+            int presidentid = GetExistingPresidentId();
+            Assume.That(presidentid > 0, "No presidents in DB, can't run test");
+            int termstart = SQLUtility.GetFirstColumnFirstRowValue("select termstart from president where presidentid = " + presidentid);
+            TestContext.WriteLine("termstart for presidentid " + presidentid + " is " + termstart);
+            termstart = termstart + 1;
+            TestContext.WriteLine("change termstart to " + termstart);
+
+            DataTable dt = President.Load(presidentid);
+            dt.Rows[0]["termstart"] = termstart;
+            President.Save(dt);
+            
+            int newtermstart = SQLUtility.GetFirstColumnFirstRowValue("select termstart from president where presidentid = " + presidentid);
+            Assert.IsTrue(newtermstart == termstart,"termstart for president (" + presidentid + ") = " + newtermstart);
+            TestContext.WriteLine("termstart for president(" + presidentid + ") = " + newtermstart);
         }
 
         [Test]
@@ -32,7 +81,7 @@ namespace RecordKeeperTest
 
         [Test]
         public void LoadPresident() {
-            int presidentid = SQLUtility.GetFirstColumnFirstRowValue("select top 1 presidentid from president");
+            int presidentid = GetExistingPresidentId();
             Assume.That(presidentid > 0, "No presidents in DB, can't run test");
             TestContext.WriteLine("existing president with id = " + presidentid);
             TestContext.WriteLine("Ensure that app loads president " + presidentid);
@@ -55,6 +104,10 @@ namespace RecordKeeperTest
             Assert.IsTrue(dt.Rows.Count == partycount,"num rows returned by app (" + dt.Rows.Count + ") <> " + partycount);
             
             TestContext.WriteLine("Number of rows in Parties return by app = " + dt.Rows.Count);
+        }
+
+        private int GetExistingPresidentId() {
+            return SQLUtility.GetFirstColumnFirstRowValue("select top 1 presidentid from president");
         }
     }
 }
