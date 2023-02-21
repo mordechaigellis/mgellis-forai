9d2f2b337d20653e019ae01b4bda5c399ffc2117
jacobfelder <jfelder@cpucodeschool.com>
2023-02-21 12:38:38 -0500

    2 17 4 - added partial match into sprocs

--- Databases/RecordKeeper/sprocs/PartyGet.sql
+++ Databases/RecordKeeper/sprocs/PartyGet.sql
@@ -1,19 +1,30 @@
-create or alter procedure dbo.PartyGet(@PartyId int = 0, @All bit = 0)
+create or alter procedure dbo.PartyGet(@PartyId int = 0, @PartyName varchar(35) = '', @All bit = 0)
 as
 begin
+	select @PartyName = nullif(@PartyName,'')
+
 	select p.PartyId, p.PartyName, p.YearStart, p.PartyColor
 	from Party p
 	where p.PartyId = @PartyId
 	or @All = 1
+	or p.PartyName like '%' + @PartyName + '%'
 	order by p.PartyName
 end
 go
+/*
 exec PartyGet
 
+exec PartyGet @PartyName = '' --no results
+
+exec PartyGet @PartyName = 'u'
+
 exec PartyGet @All = 1
+
+--exec 
 
 declare @PartyId int 
 
 select top 1 @PartyId = p.PartyId from Party p 
 
 exec PartyGet @PartyId = @PartyId
+*/
--- Databases/RecordKeeper/sprocs/PresidentGet.sql
+++ Databases/RecordKeeper/sprocs/PresidentGet.sql
@@ -1,14 +1,26 @@
-create or alter procedure dbo.PresidentGet(@PresidentId int = 0, @All bit = 0)
+create or alter procedure dbo.PresidentGet(
+	@PresidentId int = 0, 
+	@LastName varchar(100) = '',
+	@All bit = 0
+)
 as
 begin
 	select p.PresidentId, p.PartyId, p.Num, p.FirstName, p.LastName, p.DateBorn, p.DateDied, p.TermStart, p.TermEnd
 	from President p
 	where p.PresidentId = @PresidentId
 	or @All = 1
+	or (@LastName <> '' and p.LastName like '%' + @LastName + '%')
 	order by p.num
 end
 go
+
 exec PresidentGet
+
+exec PresidentGet @LastName = '' --no results
+
+exec PresidentGet @LastName = null --no results
+
+exec PresidentGet @LastName = 'm'
 
 exec PresidentGet @All = 1
 
