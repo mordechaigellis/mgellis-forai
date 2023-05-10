945644232d628a87eeeffb874490b45a06434caf
mordechaigellis <mgellis@cpucodeschool.com>
2023-05-10 20:34:47 +0000

    2 29 04 finished recording

--- Software/MAUIBasics/MAUIBasics/BasicControls.xaml.cs
+++ Software/MAUIBasics/MAUIBasics/BasicControls.xaml.cs
@@ -5,5 +5,16 @@ public partial class BasicControls : ContentPage
 	public BasicControls()
 	{
 		InitializeComponent();
+        slHeight.ValueChanged += SlHeight_ValueChanged;
 	}
+
+    private void SlHeight_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        lblMessage.Text = slHeight.Value.ToString();
+    }
+
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+        lblMessage.Text = "Record Saved - " + txtLastName.Text; 
+    }
 }
