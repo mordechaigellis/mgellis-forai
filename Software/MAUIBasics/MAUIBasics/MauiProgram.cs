de08b9f9fb0ccc18f7e8b752348a049a8ed8ec40
mordechaigellis <mgellis@cpucodeschool.com>
2023-05-24 21:30:57 +0000

    2 30 02 finished recording

--- Software/MAUIBasics/MAUIBasics/MauiProgram.cs
+++ Software/MAUIBasics/MAUIBasics/MauiProgram.cs
@@ -13,7 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("WINGDING.ttf", "WINGDING");
+					fonts.AddFont("WINGDING.ttf", "WINGDING");
             });
 
 #if DEBUG
