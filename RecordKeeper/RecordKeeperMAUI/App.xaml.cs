09837a0157beb05157d6938d0498c80026ff0c51
mordechaigellis <mgellis@cpucodeschool.com>
2023-06-04 20:34:46 +0000

    2 32 02 in middle

--- RecordKeeper/RecordKeeperMAUI/App.xaml.cs
+++ RecordKeeper/RecordKeeperMAUI/App.xaml.cs
@@ -2,11 +2,14 @@
 {
     public partial class App : Application
     {
+        public static bool LoggedIn = false;
+        public static string ConnStringSetting = "Server=.\\SQLExpress;Database=RecordKeeperDB";
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
         }
+
     }
 }
--- RecordKeeper/RecordKeeperMAUI/Login.xaml.cs
+++ RecordKeeper/RecordKeeperMAUI/Login.xaml.cs
@@ -1,3 +1,5 @@
+using RecordKeeperSystem;
+
 namespace RecordKeeperMAUI;
 
 public partial class Login : ContentPage
@@ -6,4 +8,23 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void LoginBtn_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MessageLbl.Text = "";
+            DBManager.SetConnectionString(App.ConnStringSetting,true, UserNameTxt.Text, PasswordTxt.Text);
+            App.LoggedIn = true;
+            await Navigation.PopModalAsync();
+        }
+        catch (Exception ex) {
+            MessageLbl.Text = ex.Message;
+        }
+    }
+
+    private void CancelBtn_Clicked(object sender, EventArgs e)
+    {
+        Application.Current.Quit();
+    }
 }
