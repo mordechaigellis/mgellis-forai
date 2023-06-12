50965040f7e6c55f9f7054d5695d078864a61a4d
mordechaigellis <mgellis@cpucodeschool.com>
2023-06-12 18:21:14 +0000

    2 33 3 finished recording

--- Software/OOPBasics/OOPBasics/MainPage.xaml.cs
+++ Software/OOPBasics/OOPBasics/MainPage.xaml.cs
@@ -7,5 +7,31 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void PersonBtn_Clicked(object sender, EventArgs e)
+    {
+		Person p = new();
+		p.FirstName = "John";
+		p.LastName = "Smith";
+		p.Gender = Person.GenderEnum.Male;
+		p.DOB = DateTime.Now.AddYears(-20);
+
+        Person p2 = new();
+        p2.FirstName = "Sue";
+        p2.LastName = "Jones";
+        p2.Gender = Person.GenderEnum.Female;
+		p2.DOB = DateTime.Now.AddYears(-50);
+
+		DisplayPerson(p);
+		DisplayPerson(p2);
+    }
+
+	private void DisplayPerson(Person person) {
+		DisplayValue(person.Description);
+	}
+	private void DisplayValue(string value) {
+		DisplayLbl.Text = DisplayLbl.Text + Environment.NewLine + "--------" + Environment.NewLine + value;
+	}
+
 }
 
---
+++ Software/OOPBasics/OOPBasics/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasics
+{
+    public class Person
+    {
+        public enum GenderEnum { Unknown, Male, Female}
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public GenderEnum Gender { get; set; } = GenderEnum.Unknown;
+
+        public DateTime DOB { get; set; }
+
+        public string Description
+        {
+            get
+            {
+                string desc = $"{this.FirstName} {this.LastName} {this.Gender.ToString()} {this.Age} years old";
+
+                return desc;
+            }
+        }
+
+        public int Age { 
+            get { return DateTime.Now.Year - DOB.Year; } 
+        }
+
+
+      
+    }
+}
