1864baeb818b913f6fd3dab4c467c236a5a4807e
mordechaigellis <mgellis@cpucodeschool.com>
2023-06-20 21:29:01 +0000

    2 36 02 in middle

---
+++ Software/Desktop/TicTacToe/TicTacToeSystem/Game.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToeSystem
+{
+    public class Game
+    {
+
+    }
+}
---
+++ Software/Desktop/TicTacToe/TicTacToeSystem/Spot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeSystem
+{
+    public class Spot
+    {
+        private GameStatusEnum _gamestatus;
+        public enum GameStatusEnum { NotStarted, Playing, Winner, Tie}
+
+        public GameStatusEnum GameStatus {
+            get => _gamestatus;
+            set {
+                _gamestatus = value;
+            }
+
+        }
+    }
+}
