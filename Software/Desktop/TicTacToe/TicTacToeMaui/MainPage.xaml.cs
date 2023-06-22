2dc1e95535971c64f7dc3670cc2a345e072856dc
mordechaigellis <mgellis@cpucodeschool.com>
2023-06-22 16:49:29 +0000

    2 36 05 finished recording

---
+++ Software/Desktop/TicTacToe/TicTacToeMaui/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using TicTacToeSystem;
+namespace TicTacToeMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        Game game = new();
+        List<Button> lstbuttons;
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = game;
+            lstbuttons = new() { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+        }
+
+        private void StartBtn_Clicked(object sender, EventArgs e)
+        {
+            game.StartGame();
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            game.TakeSpot(lstbuttons.IndexOf((Button)sender));
+        }
+    }
+}
--- Software/Desktop/TicTacToe/TicTacToeSystem/Spot.cs
+++ Software/Desktop/TicTacToe/TicTacToeSystem/Spot.cs
@@ -33,7 +33,12 @@ namespace TicTacToeSystem
             {
                 _backcolor = value;
                 this.InvokePropertyChanged();
+                this.InvokePropertyChanged("BackColorMaui");
             }
+        }
+
+        public Microsoft.Maui.Graphics.Color BackColorMaui {
+            get => this.ConvertToMauiColor(this.BackColor);
         }
 
         internal int Priority { get; set; }
@@ -48,6 +53,16 @@ namespace TicTacToeSystem
         private void InvokePropertyChanged([CallerMemberName] string propertyname = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        private Microsoft.Maui.Graphics.Color ConvertToMauiColor(System.Drawing.Color systemColor)
+        {
+            float red = systemColor.R / 255f;
+            float green = systemColor.G / 255f;
+            float blue = systemColor.B / 255f;
+            float alpha = systemColor.A / 255f;
+
+            return new Microsoft.Maui.Graphics.Color(red, green, blue, alpha);
         }
     }
 }
