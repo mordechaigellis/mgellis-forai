e4db370e55239a3f053676001897509e250e7c2d
mordechaigellis <mgellis@cpucodeschool.com>
2023-07-23 19:12:03 +0000

    2-32-03-fix finished recording

--- RecordKeeper/RecordKeeperMAUI/PresidentSearch.xaml.cs
+++ RecordKeeper/RecordKeeperMAUI/PresidentSearch.xaml.cs
@@ -1,3 +1,6 @@
+using RecordKeeperSystem;
+using System.Data;
+
 namespace RecordKeeperMAUI;
 
 public partial class PresidentSearch : ContentPage
@@ -6,4 +9,14 @@ public partial class PresidentSearch : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void SearchPresidents() {
+		DataTable dt = President.SearchPresidents(0, LastNameTxt.Text, 0, 0);
+		PresidentLst.ItemsSource = dt.Rows;
+	}
+
+    private void SearchBtn_Clicked(object sender, EventArgs e)
+    {
+		SearchPresidents();
+    }
 }
