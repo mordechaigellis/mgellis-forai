2ca8fc22961c24c93b5c5eda4a88511e38470be4
mordechaigellis <mgellis@cpucodeschool.com>
2023-09-19 21:15:21 +0000

    session material

--- Sessions Part 3/05/2 arrays.js
+++ Sessions Part 3/05/2 arrays.js
@@ -1,1 +1,28 @@
-//1
+//for all console.log(array variable)
+
+//1 declare array with 4 numbers non-sorted
+
+//2 sort the array
+
+//3 return an array that only has the 2 middle items
+
+//4 add the next 3 sequences
+
+//5 replace 3 thru 5th item with num spelled out
+
+//6 return a new array only with the spelled out values
+
+//7 return a new array that has all the numeric values doubled
+
+//8 use forEach to output the index of each item (indexOf) and it's value
+
+//9 using a for loop show the ascii value for each char in "the twain shall never meet"
+
+//10 loop from 25 to 40 and build a string of chars using the String.fromCharCode method
+
+//11 use the join method to produce the following array as one string of values 'h','a','v,'i','n',g,' ','f','u','n'
+
+//12 destructure 1st 2 elements into two vars
+
+//13 same as 12 and get the rest of the values into a new array
+
---
+++ Sessions Part 3/05/HW.js
@@ -0,0 +1,8 @@
+/*
+Write a function called xcrypt that takes a phrase and can either encrypt or decrypt it.
+The encryption method is to change each character in the phrase to the next specified number of characters in the ascii chart.
+For example if the prhase it "hello world" and the user specifies 5 then the result would be "jgnnq"yqtnf"
+because j has the ascii value of h + 2, and so on for the rest of the chars.
+You should be able to call the function like this: let x = xcrypt("hello world",2) to encrypt and xcrypt(x,-2) to decrypt
+*/
+
