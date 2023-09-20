2b6d520947f50fa3ee624e5726bcda54d06651eb
mordechaigellis <mgellis@cpucodeschool.com>
2023-09-20 16:55:40 +0000

    3 05 4 finished recording

---
+++ Sessions Part 3/05/3 put it all togeher.js
@@ -0,0 +1,10 @@
+/*
+You are writing softare to transmit data over a network that has the following rules:
+a) The the data has to be sent in single characters.
+b) After every 3 chars transmit ---.
+c) Ensure that undefined, null, and NaN are not sent, they will crash the system.
+
+Write a JS function that take a string and transmits the data following the specified rules.
+In place of sending the data on the network console.log it.
+
+*/
--- Software/js-node/2 arrays.js
+++ Software/js-node/2 arrays.js
@@ -19,18 +19,36 @@ console.log("push",a);
 a.splice(2,3,'six','eight','nine');
 console.log("splice",a);
 //6 return a new array only with the spelled out values
-
+let a2 = a.filter(n => isNaN(n));
+console.log("filter",a2)
 //7 return a new array that has all the numeric values doubled
-
+a2 = a.map(n => n = n*2);
+console.log(a2);
 //8 use forEach to output the index of each item (indexOf) and it's value
+a.forEach(n => console.log("index",a.indexOf(n),"value",n));
 
 //9 using a for loop show the ascii value for each char in "the twain shall never meet"
-
+let s = "the twain shall never meet";
+let c = s.split("");
+for(let n of c){
+    console.log(n,n.charCodeAt(0));    
+}
+//console.log("split string",c)
 //10 loop from 25 to 40 and build a string of chars using the String.fromCharCode method
-
+s = "";
+for(let i = 25;i < 41;i++){
+    s = s + String.fromCharCode(i);
+}
+console.log("loop from 25 to 40 to generate a string",s);
 //11 use the join method to produce the following array as one string of values 'h','a','v,'i','n',g,' ','f','u','n'
-
+let v = ['h','a','v','i','n','g',' ','f','u','n']
+s = v.join("");
+console.log("join",s);
 //12 destructure 1st 2 elements into two vars
-
+//console.log(a[2]);
+let n1;let n2; let narr;
+[n1,n2] = a;
+console.log("n1 n2",n1, n2);
 //13 same as 12 and get the rest of the values into a new array
-
+[n1,n2,...narr] = a;
+console.log("n1 n2 narr",n1, n2,narr);
