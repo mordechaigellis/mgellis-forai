1c706083f23172b61c8c831244267a60352164be
mordechaigellis <mgellis@cpucodeschool.com>
2023-09-20 18:34:18 +0000

    session material

--- Sessions Part 3/05/3 put it all togeher.js
+++ Sessions Part 3/05/3 put it all togeher.js
@@ -1,10 +1,11 @@
 /*
 You are writing softare to transmit data over a network that has the following rules:
-a) The the data has to be sent in single characters.
-b) After every 3 chars transmit ---.
-c) Ensure that undefined, null, and NaN are not sent, they will crash the system.
+a) First trasmit the number of chars in the string to be transmitted followed by :
+b) The data has to be sent in single characters.
+c) After every 3 chars transmit ---.
+d) when finished transmit :end
+d) Beware of undefined, null, and NaN that may be passed to your system for transmitting. If you receive that then transmit "INVALID".
 
 Write a JS function that take a string and transmits the data following the specified rules.
 In place of sending the data on the network console.log it.
-
 */
