4f0781f2c4e457c2cc1d4bd1883a379dd5688ecc
mordechaigellis <m@michaelgellis.com>
2023-10-25 14:09:52 -0400

    3-08 session material

---
+++ Sessions Part 3/08/1 short-circuit assigment.js
@@ -0,0 +1,21 @@
+let x; let s; let br = "\n------------------------";
+/*
+x is an account balance when 0 then output "no money" else ouptut the balance, show value of x and s for both cases
+ */
+console.log("x is an account balance.\nwhen 0 then output 'no money'\nelse ouptut the balance", br);
+//code her for when x is falsy
+console.log("when x = ", x, "then s =", s, br)
+
+//code her for when x is truthy
+console.log("when x = ", x, "then s =", s, br)
+console.log("***********************")
+
+//x is a winning lottery num when not zero output winner else x
+console.log("x is a winning lottery num when not zero output winner else x", br);
+//code her for when x is falsy
+console.log("when x = ", x, "then s =", s, br)
+
+//code her for when x is falsy
+console.log("when x = ", x, "then s =", s, br)
+
+//
