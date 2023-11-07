04c45480d695bc9201f9210d22e70a88a48b4db0
mordechaigellis <mgellis@cpucodeschool.com>
2023-11-07 19:13:39 +0000

    3-10-04 finished recording

---
+++ Software/JSWebBasics/JSWebBasics/Pages/TicTacToe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSWebBasics.Pages
+{
+    public class TicTacToeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
