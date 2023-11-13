99cd14aeaebc80af0937207aeb069b6480146f0e
mordechaigellis <mgellis@cpucodeschool.com>
2023-11-13 17:31:04 +0000

    3-11-1 finished recording

---
+++ Software/JSWebBasics/JSWebBasics/Pages/basicsJquery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSWebBasics.Pages
+{
+    public class basicsJqueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
