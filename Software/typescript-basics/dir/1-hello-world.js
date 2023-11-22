36b14475bf0a81293d64ec389106c0c9c34e893e
mordechaigellis <mgellis@cpucodeschool.com>
2023-11-22 20:51:31 +0000

    3-12-4 finished recording

--- Software/typescript-basics/dir/1-hello-world.js
+++ Software/typescript-basics/dir/1-hello-world.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let s;
 let n = 0;
+let x;
+x = "a";
+console.log(x, typeof (x));
+x = 1;
+console.log(x, typeof (x));
 console.log(n * n);
 s = "hello world";
 console.log(s);
--- Software/typescript-basics/src/1-hello-world.ts
+++ Software/typescript-basics/src/1-hello-world.ts
@@ -1,10 +1,21 @@
 let s: string;
 let n = 0;
+let x;
+
+x = "a";
+console.log(x, typeof(x));
+x = 1;
+console.log(x, typeof(x));
+s = "a";
+//x = x * s;
 
 console.log(n*n);
 
 s = "hello world";
 console.log(s);
 
+function logit(val: any, desc = ""){
+    
+}
 
 export {};
