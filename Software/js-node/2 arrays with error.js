9088c2479df76e7790988ce632721232f1641924
mordechaigellis <mgellis@cpucodeschool.com>
2023-11-22 17:13:32 +0000

    3-12-1 finished recording

---
+++ Software/js-node/2 arrays with error.js
@@ -0,0 +1,9 @@
+//for all console.log(array variable)
+
+//1 declare array with 4 numbers non-sorted
+let a = [8,2,6,1];
+console.log(a);
+a = 0;
+//2 sort the array
+a.sort();
+console.log('sort',a);
