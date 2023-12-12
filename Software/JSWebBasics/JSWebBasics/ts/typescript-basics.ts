b90419c57bf3dc3a8179c9f64d613a00e40e55ce
mordechaigellis <mgellis@cpucodeschool.com>
2023-12-12 17:50:00 +0000

    3-13-02 finished recording

--- Software/JSWebBasics/JSWebBasics/ts/typescript-basics.ts
+++ Software/JSWebBasics/JSWebBasics/ts/typescript-basics.ts
@@ -1,1 +1,19 @@
-﻿console.log("hi how are you! fine thank you.");
+﻿const msg: HTMLElement = document.querySelector("#msg");
+document.querySelector("#btn1").addEventListener("click", displayMillisecond);
+document.querySelector("#btndivs").addEventListener("click", changeDivs)
+const divs = document.querySelectorAll("div");
+function displayMillisecond() {
+    let color: string = "";
+    msg.innerHTML = new Date().getMilliseconds().toString();
+    if ((document.querySelector("#optblue") as HTMLInputElement).checked) {
+        color = (document.querySelector("#optblue") as HTMLInputElement).value;
+    }
+    else {
+        color = (document.querySelector("#optred") as HTMLInputElement).value;
+    }
+    msg.style.backgroundColor = color;
+}
+
+function changeDivs() {
+    divs.forEach(d => d.style.backgroundColor = "silver");
+}
--- Software/JSWebBasics/JSWebBasics/wwwroot/js/typescript-basics.js
+++ Software/JSWebBasics/JSWebBasics/wwwroot/js/typescript-basics.js
@@ -1,2 +1,19 @@
-console.log("hi how are you! fine thank you.");
+const msg = document.querySelector("#msg");
+document.querySelector("#btn1").addEventListener("click", displayMillisecond);
+document.querySelector("#btndivs").addEventListener("click", changeDivs);
+const divs = document.querySelectorAll("div");
+function displayMillisecond() {
+    let color = "";
+    msg.innerHTML = new Date().getMilliseconds().toString();
+    if (document.querySelector("#optblue").checked) {
+        color = document.querySelector("#optblue").value;
+    }
+    else {
+        color = document.querySelector("#optred").value;
+    }
+    msg.style.backgroundColor = color;
+}
+function changeDivs() {
+    divs.forEach(d => d.style.backgroundColor = "silver");
+}
 //# sourceMappingURL=typescript-basics.js.map
