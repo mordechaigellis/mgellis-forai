67a9c624a869e95c282d034297ec4cc6350bd6e3
mordechaigellis <mgellis@cpucodeschool.com>
2023-12-26 20:02:36 +0000

    2-14-2 finished recording

--- Software/JSWebBasics/JSWebBasics/ts/fetchbasics.ts
+++ Software/JSWebBasics/JSWebBasics/ts/fetchbasics.ts
@@ -1,9 +1,20 @@
 ﻿type post = { id: number, body: string, title?: string, userId: number };
 
-let p: post = { id: 1, body: "hello world", userId: 10 };
+let p: post; //= { id: 1, body: "hello world", userId: 10 };
+let num = 1;
 let msg2: HTMLElement = document.querySelector("#msg") ;
 document.querySelector("#btn").addEventListener("click", btnclick)
 
-function btnclick() {
+async function btnclick() {
+    p = await fetchFromAPI(`https://jsonplaceholder.typicode.com/posts/${num}`);
+    num++;
     msg2.innerHTML = p.body;
+    //
+}
+
+async function fetchFromAPI<T>(url: string): Promise<T> {
+    let o: T;
+    let r = await fetch(url);
+    o = await r.json();
+    return o;
 }
--- Software/JSWebBasics/JSWebBasics/wwwroot/js/fetchbasics.js
+++ Software/JSWebBasics/JSWebBasics/wwwroot/js/fetchbasics.js
@@ -1,7 +1,30 @@
-let p = { id: 1, body: "hello world", userId: 10 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let p; //= { id: 1, body: "hello world", userId: 10 };
+let num = 1;
 let msg2 = document.querySelector("#msg");
 document.querySelector("#btn").addEventListener("click", btnclick);
 function btnclick() {
-    msg2.innerHTML = p.body;
+    return __awaiter(this, void 0, void 0, function* () {
+        p = yield fetchFromAPI(`https://jsonplaceholder.typicode.com/posts/${num}`);
+        num++;
+        msg2.innerHTML = p.body;
+        //
+    });
+}
+function fetchFromAPI(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let o;
+        let r = yield fetch(url);
+        o = yield r.json();
+        return o;
+    });
 }
 //# sourceMappingURL=fetchbasics.js.map
