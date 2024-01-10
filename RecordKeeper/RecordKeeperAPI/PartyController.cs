31ee1ae95b3eff601e4df5eebb63a85cfb1fd357
mordechaigellis <mgellis@cpucodeschool.com>
2024-01-10 21:58:18 +0000

    3-15-3 in process

---
+++ RecordKeeper/RecordKeeperAPI/PartyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecordKeeperAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartyController : ControllerBase
+    {
+
+    }
+}
