937c9a670523831530a54c364daa402f5656e658
mordechaigellis <mgellis@cpucodeschool.com>
2024-01-10 19:34:25 +0000

    3-15-2 finished recording

---
+++ Software/JSWebBasics/APIBasics/APIBasics/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIBasics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        List<Customer> lst = new() { new() { Id = 1, Name = "Andy" }, new() { Id = 2, Name = "Bob", Category = Customer.CategoryEnum.priority } };
+
+        [HttpGet]
+        [HttpGet("getcust")]
+        public List<Customer> Get() {
+            return lst;
+        }
+        [HttpGet("{id:int:range(1,2)}")]
+        public Customer GetCustomer(int id) {
+            return lst.First(c => c.Id == id);
+        }
+
+        [HttpGet("{id}/{category}")]
+        [HttpGet("id/{id}/category/{category}")]
+        public Customer GetCustomerByIdAndCategory(int id, string category)
+        {
+            return lst.First(c => c.Id == id && c.Category.ToString().ToLower() == category.ToLower());
+        }
+
+        [HttpGet("qs")]
+        public Customer GetCustomerByIdAndCategoryAndQueryString(int id, string category)
+        {
+            return lst.First(c => c.Id == id && c.Category.ToString().ToLower() == category.ToLower());
+        }
+
+    }
+}
---
+++ Software/JSWebBasics/APIBasics/APIBasics/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace APIBasics
+{
+    public class Customer
+    {
+        public enum CategoryEnum { standard, priority }
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+
+        public CategoryEnum Category { get; set; } = CategoryEnum.standard;
+    }
+}
