5661333ba9178234294cdea4c77c1f32ef456425
mordechaigellis <mgellis@cpucodeschool.com>
2024-03-05 19:43:28 +0000

    3-19-3 in process

--- Software/react-basics/src/App.tsx
+++ Software/react-basics/src/App.tsx
@@ -7,17 +7,40 @@ import { useState } from "react"
 
 function App() {
   const [presidents, setPresidents] = useState(initialPresidents);
+  const [featured, setFeatured] = useState(0);
+
+  const getCopyOfPresidents = () => presidents.map(p => ({ ...p }));
 
   const handleChangePrez = () => {
-    if (presidents.length > 0) {
-      const newPresidents = presidents.map(p => ({ ...p }));
+    const newPresidents = getCopyOfPresidents();
+    if (newPresidents.length > 0) {
       newPresidents[0].lastName = newPresidents[0].lastName + "A";
       console.log("current", presidents[0].lastName);
       console.log("new", newPresidents[0].lastName);
       setPresidents(newPresidents);
     }
-
   };
+
+  const handleAddPresident = () => {
+    const newPresidents = getCopyOfPresidents();
+    if (newPresidents.length > 0) {
+      const newPrez = { ...newPresidents[newPresidents.length - 1] };
+      newPrez.lastName += "B";
+      newPrez.num++;
+      newPresidents.push(newPrez);
+      setPresidents(newPresidents);
+
+    }
+  };
+
+  const handleChangeFeatured = () => {
+    let newFeatured = featured;
+    newFeatured++;
+    if (newFeatured <= presidents.length - 1) {
+      setFeatured(newFeatured);
+    }
+
+  }
 
   return (
     <div className="container">
@@ -29,6 +52,7 @@ function App() {
       </div>
       <div className="row">
         <div className="col-6">
+
           <div className="row">
             <div className="col-12">
               <PresidentSummary presidents={presidents} />
@@ -38,6 +62,10 @@ function App() {
             <div className="col-3">
               <button onClick={handleChangePrez} className="btn btn-primary">Change Prez</button>
             </div>
+            <div className="col-3">
+              <button onClick={handleAddPresident} className="btn btn-primary">Add Prez</button>
+            </div>
+
           </div>
           <div className="row">
             <div className="col-12">
@@ -47,8 +75,12 @@ function App() {
         </div>
         <div className="col-6">
           <div className="row">
+            <div className="col-4"><button className="btn btn-success">Back</button></div>
+            <div className="col-4"><button onClick={handleChangeFeatured} className="btn btn-success">Next {featured}</button></div>
+          </div>
+          <div className="row">
             <div className="col-12">
-              <PresidentFeature president={presidents[0]} />
+              <PresidentFeature president={presidents[featured]} />
             </div>
           </div>
           <div className="row">
