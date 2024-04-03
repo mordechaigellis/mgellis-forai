d60198db74c123d0a2ee325fb3e925ca1dca20c1
mordechaigellis <mgellis@cpucodeschool.com>
2024-04-03 21:51:20 +0000

    3-22-4 finished recording

--- Software/recordkeeper-react/src/PartyButton.tsx
+++ Software/recordkeeper-react/src/PartyButton.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react"
+
 import { IParty } from "./DataInterfaces"
 
 interface Props {
-    party: IParty
+    party: IParty,
+    isSelected: boolean
+    onSelected: (partyId: number) => void
 }
-export default function PartyButton({ party }: Props) {
-    const [selected, setSelected] = useState(false);
-
+export default function PartyButton({ party, isSelected, onSelected }: Props) {
     return (
-        <div onClick={ } className = {`btn ${selected ? "bg-secondary" : ""}`}>
+        <div onClick={() => onSelected(party.partyId)} className={`btn ${isSelected ? "bg-secondary" : ""}`}>
             <figure className="figure">
                 <img src={`/public/images/party-images/${party.partyName}.jpg`} className="figure-img img-fluid rounded" alt="..." />
                 <figcaption className="figure-caption text-center">{party.partyName}</figcaption>
--- Software/recordkeeper-react/src/SideBar.tsx
+++ Software/recordkeeper-react/src/SideBar.tsx
@@ -5,17 +5,28 @@ import PartyButton from "./PartyButton";
 
 export default function Sidebar() {
     const [partylist, setPartyList] = useState<IParty[]>([]);
+    const [selectedPartyId, setSelectedPartyId] = useState(0);
+
     useEffect(() => {
         const fetchdata = async () => {
             const data = await fetchParties();
             setPartyList(data);
+            if (data.length > 0) {
+                handleSelectedParty(data[0].partyId);
+            }
         }
         fetchdata();
     }
         , []);
+
+    function handleSelectedParty(partyId: number) {
+        setSelectedPartyId(partyId);
+    }
+
     return (
         <>
-            <h2>{partylist.map(p => <PartyButton party={p} />)}</h2>
+            <h2>{partylist.map(p =>
+                <PartyButton key={p.partyId} party={p} onSelected={handleSelectedParty} isSelected={p.partyId == selectedPartyId} />)}</h2>
         </>
     )
 }
