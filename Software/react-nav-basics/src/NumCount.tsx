f4d09053474eb2bf8d19f7a0afc1f9f93bfb7959
mordechaigellis <mgellis@cpucodeschool.com>
2024-05-07 19:25:03 +0000

    3-25-5 finished recording

--- Software/react-nav-basics/src/NumCount.tsx
+++ Software/react-nav-basics/src/NumCount.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function NumCount({ num }: Props) {
     let s = "";
-    for (let i = 0; i <= num; i++) {
+    for (let i = 1; i <= num; i++) {
         s = s + i + ", ";
     }
     return (
--- Software/react-nav-basics/src/Screen3.tsx
+++ Software/react-nav-basics/src/Screen3.tsx
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom"
+import NumStars from "./NumStars";
+import NumCount from "./NumCount";
 
 
 export default function Screen3() {
     const { val, num } = useParams();
-    
+    const numvalue = Number(num) || 0;
+
     return (
         <>
-            <div>Screen3 dynamic path val = {val}</div>
+            <div>Screen3 dynamic path val = {val} num = {num}</div>
+            <hr />
+            <NumStars num={numvalue} />
+            <hr />
+            <NumCount num={numvalue} />
         </>
     )
 }
