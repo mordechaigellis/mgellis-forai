06df9eaf2ce61a5332eb6f15bb962a309112a9d5
mordechaigellis <mgellis@cpucodeschool.com>
2024-05-23 19:35:34 +0000

    3-26-6 in process

--- Software/react-nav-basics/src/Screen1.tsx
+++ Software/react-nav-basics/src/Screen1.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-
+const keyname = "key";
+const value = "hello world";
 
 export default function Screen1() {
     const navigate = useNavigate();
+    const handleSaveClick = () => {
+        sessionStorage.setItem(keyname, value + "- session");
+        localStorage.setItem(keyname, value + "- local");
+    }
+    const handleRemoveClick = () => {
+        sessionStorage.removeItem(keyname);
+        localStorage.removeItem(keyname);
+    }
     return (
         <>
             <h1>Screen1</h1>
+            <hr />
+            <button onClick={handleSaveClick}>Save value to storage</button>
+            <button onClick={handleRemoveClick}>Remove value storage</button>
+            <hr />
             <button onClick={() => navigate("/screen2")}>Nav to Screen 2</button>
         </>
     )
