cffc30f5ee211fed8ac9892caba0b9e9f5c25885
mordechaigellis <mgellis@cpucodeschool.com>
2024-06-05 18:18:35 +0000

    3-27-2 finished recording

--- Software/recordkeeper-react/src/DataUtil.ts
+++ Software/recordkeeper-react/src/DataUtil.ts
@@ -1,53 +1,28 @@
 import { FieldValues } from "react-hook-form";
 import { IColor, IParty, IPresident } from "./DataInterfaces";
+import { createAPI } from "@charliecpu/reactutils";
 
 let baseurl = import.meta.env.VITE_API_URL;
-
-async function fetchData<T>(url: string): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url);
-    const data = await r.json();
-    return data;
-}
-
-async function deleteData<T>(url: string): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, { method: "DELETE" });
-    const data = await r.json();
-    return data;
-}
-
-async function postData<T>(url: string, form: FieldValues): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(form),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    const data = await r.json();
-    return data;
-};
+const api = createAPI(baseurl);
 
 export async function fetchParties() {
-    return await fetchData<IParty[]>("party");
+    return await api.fetchData<IParty[]>("party");
 }
 
 export async function fetchPresidentByPartyId(partyId: number) {
-    return await fetchData<IPresident[]>(`president/getbyparty/${partyId}`);
+    return await api.fetchData<IPresident[]>(`president/getbyparty/${partyId}`);
 }
 
 export async function fetchColors() {
-    return await fetchData<IColor[]>("Party/colors");
+    return await api.fetchData<IColor[]>("Party/colors");
 }
 
 export async function postParty(form: FieldValues) {
-    return postData<IParty>("party", form);
+    return api.postData<IParty>("party", form);
 }
 
 export async function deleteParty(partyId: number) {
-    return deleteData<IParty>(`party?id=${partyId}`);
+    return api.deleteData<IParty>(`party?id=${partyId}`);
 
 }
 
