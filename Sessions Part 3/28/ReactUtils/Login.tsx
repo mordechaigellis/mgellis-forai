493a2d5c058f3c5928da98c806d314da251f6710
mordechaigellis <mgellis@cpucodeschool.com>
2024-06-24 17:20:49 +0000

    3-28-3 session material

---
+++ Sessions Part 3/28/ReactUtils/Login.tsx
@@ -0,0 +1,30 @@
+import { useForm } from "react-hook-form"
+import { useUserStore } from "@charliecpu/reactutils";
+
+type forminput = { username: string, password: string }
+
+export default function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm<forminput>();
+    const login = useUserStore((state) => state.login);
+    const isLoggedIn = useUserStore(state => state.isLoggedIn);
+    const onSubmit = async (data: forminput) => await login(data.username, data.password);
+
+    return (
+        <>
+            <div>Logged In = {isLoggedIn.toString()}</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Username</label>
+                <input type="text" {...register('username', { required: 'Username is required' })} />
+                {errors.username && <span>{errors.username.message}</span>}
+                <br />
+                <label>Password</label>
+                <input type="password"
+                    {...register('password', { required: 'Password is required' })} />
+                {errors.password && <span>{errors.password.message}</span>}
+                <br />
+                <button type="submit">Login</button>
+
+            </form>
+        </>
+    )
+}
